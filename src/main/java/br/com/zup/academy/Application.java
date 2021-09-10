a3638b118729b7644407cfb112f828d239e71a5e
unknown <diegohenrique@gmail.com>
2021-09-10 15:37:53 -0300

    first base commit

---
+++ src/main/java/br/com/zup/academy/Application.java
@@ -0,0 +1,10 @@
+package br.com.zup.academy;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
